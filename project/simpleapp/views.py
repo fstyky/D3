9bd717c93986e8f25b7dd6a10ef310567f0ba080
fstyky <116486128+fstyky@users.noreply.github.com>
2022-12-28 12:31:34 +0300

    Add files via upload

---
+++ project/simpleapp/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Product,Post
+from datetime import datetime
+
+# Create your views here.
+
+class ProductsList(ListView):
+    model = Product
+    ordering = 'name'
+    # queryset = Product.objects.filter(
+    #    price__lt= 300
+    # ).order_by('name')
+    template_name = 'products.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = 'Распродажа в среду'
+        return context
+
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product'
+    pk_url_kwarg = 'id'
+
+class PostsList(ListView):
+    model = Post
+    ordering = 'title'
+    # queryset = Product.objects.filter(
+    #    price__lt= 300
+    # ).order_by('name')
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = 'Распродажа в среду'
+        return context
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
